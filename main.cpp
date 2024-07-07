1ba018a5240b992754e4655848440972139de436
Yuan Li <4112003+lydxlx1@users.noreply.github.com>
2024-07-07 12:53:38 -0700

    Init repo

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+int main(void) {
+    cout << "hehe" << endl;
+    return 0;
+}
